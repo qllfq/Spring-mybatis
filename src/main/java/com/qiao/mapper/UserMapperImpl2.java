4bdcb6d8120ce1a040b3cad4a30b39626c02bf09
qllfq <qll2353695310@163.com>
2020-11-03 21:25:28 +0800

    继承sqlSessionDaoSupport

---
+++ src/main/java/com/qiao/mapper/UserMapperImpl2.java
@@ -0,0 +1,13 @@
+package com.qiao.mapper;
+
+import com.qiao.pojo.User;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import java.util.List;
+
+public class UserMapperImpl2 extends SqlSessionDaoSupport implements UserMapper{
+    public List<User> selectUser(){
+        UserMapper mapper = getSqlSession().getMapper(UserMapper.class);
+        return mapper.selectUser();
+    }
+}
